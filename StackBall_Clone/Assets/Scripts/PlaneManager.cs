03591da005d36fdc77fd63c88f197d4dcbac533d
JIIIIIND <jinwoo700@gmail.com>
2021-04-01 03:32:19 +0900

    기본 틀 제작 - plane 회전 완료 - 공 튀기기 동작 구현 완료 - 클릭/터치 시 plane 제거 동작 완료
    수정 필요
    - 회전 속도 및 plane의 개수

---
+++ StackBall_Clone/Assets/Scripts/PlaneManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _planeList;
+    [SerializeField]
+    private List<GameObject> _planeModel;
+    [SerializeField]
+    private float rotateValue;
+    private int select;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        select = Random.Range(0, 9);
+        Debug.Log(select);
+        UpdatePlane(select);
+    }
+
+    void UpdatePlane(int model)
+    {
+        // Y 값 18.6 부터 0.4씩 떨어트리면서 0.0까지 생성
+        float rotate = 0.0f;
+        for (float y = 18.6f; y >= 0.0f; y -= 0.4f)
+        {
+            GameObject gameObject = Instantiate(_planeModel[model], new Vector3(0, y, 0), Quaternion.identity);
+            gameObject.transform.Rotate(Vector3.up, rotate);
+            rotate -= rotateValue * Time.deltaTime;
+            gameObject.transform.parent = _planeList.transform;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ StackBall_Clone/Assets/Scripts/TouchEvent.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchEvent : MonoBehaviour
+{
+    private Collider    _coll;
+    private bool        _gameEnd;
+    
+    private bool isFirst = false;
+    private Vector3 _savedVelocity;
+    private Rigidbody _rig;
+    [SerializeField]
+    private float _downVelocity;
+    [SerializeField]
+    private float _bounce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _coll = GetComponent<Collider>();
+        _rig = GetComponent<Rigidbody>();
+        _rig.velocity = new Vector3(0, _bounce, 0);
+        _savedVelocity = _rig.velocity;
+        _gameEnd = false;
+    }
+
+    private void CheckTouch()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Moved ||
+                touch.phase == TouchPhase.Stationary)
+            {
+                _coll.isTrigger = true;
+            }
+            else if (touch.phase == TouchPhase.Canceled ||
+                touch.phase == TouchPhase.Ended)
+            {
+                _coll.isTrigger = false;
+            }
+        }
+        if (!_gameEnd)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                _coll.isTrigger = true;
+            }
+            if (Input.GetMouseButtonUp(0))
+            {
+                _coll.isTrigger = false;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        _rig.velocity = new Vector3(0, _downVelocity, 0);
+        if (other.gameObject.tag == "Plane")
+            Destroy(other.gameObject);
+        else
+        {
+            _gameEnd = true;
+            _coll.isTrigger = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        _rig.velocity = _savedVelocity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckTouch();
+    }
+}
